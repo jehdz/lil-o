b8adea722cabe8b4317e62978b87d7e3911bed1a
Jose Hernandez <jose.hernandez@abt.com>
2022-03-06 19:26:22 -0600

    project structure - form

---
+++ app.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const debug = require('debug')('app');
+const morgan = require('morgan');
+const path = require('path');
+const passport = require('passport');
+const cookieParser = require('cookie-parser');
+const session = require('express-session');
+const { render } = require('ejs');
+
+
+const PORT = process.env.PORT || 3000;
+const app = express();
+
+
+// middleware 
+
+app.use(morgan('tiny'));
+app.use(express.static(path.join(__dirname, '/public/')));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+// for passport js --> user authenthication 
+app.use(cookieParser());
+app.use(session({secret: 'captured-content', resave: true, saveUninitialized: true }));
+
+
+
+// require('./src/config/passport.js')(app);
+
+app.set('views', './src/views');
+app.set('view engine', 'ejs');
+
+
+app.get("/", (req, res) => {
+    res.render('index', {title: 'This is working properly', data: ['a', 'b', 'c']})
+})
+
+
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port http://localhost:${PORT}`)
+})
